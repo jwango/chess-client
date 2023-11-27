aab318e93834bde23365dbf036b667df00052604
jwango <joshua.wang.h@gmail.com>
2023-11-27 11:34:42 -0500

    feat: sort inputs

--- src/modules/chess/game/MoveInputField.tsx
+++ src/modules/chess/game/MoveInputField.tsx
@@ -1,6 +1,6 @@
-import { GameMove } from "../lib/dto";
+import { GameMove, GameMoveType } from "../lib/dto";
 import { Select, SelectOption } from "@shared";
-import { PieceIndex, getPieceIndex } from "../lib/util";
+import { PieceIndex, getPieceIndex, getSpaceCoord } from "../lib/util";
 
 export interface MoveInputFilters {
   piece: PieceIndex;
@@ -16,9 +16,11 @@ interface MoveInputFieldProps {
 export const MoveInputField = ({ moves, filters, onFilter, onSelect }: MoveInputFieldProps) => {
   const movesByPiece = getMovesByPiece(moves);
   const pieceIndices = Object.keys(movesByPiece);
-  const pieceOptions: SelectOption<PieceIndex>[] = (pieceIndices || [])?.map(pieceIndex => (
-    { id: pieceIndex, name: pieceIndex, val: pieceIndex }
-  ));
+  const pieceOptions: SelectOption<PieceIndex>[] = (pieceIndices || [])
+    ?.map(pieceIndex => (
+      { id: pieceIndex, name: pieceIndex, val: pieceIndex }
+    ))
+    ?.sort((a, b) => a.name.localeCompare(b.name));
 
   const currPiece = filters.piece;
   const moveOptions = (movesByPiece[currPiece] || [])?.map((move, i) => {
@@ -49,16 +51,50 @@ export const MoveInputField = ({ moves, filters, onFilter, onSelect }: MoveInput
 type PieceMoveMap = Partial<Record<PieceIndex, GameMove[]>>;
 
 function getMovesByPiece(moves: GameMove[]): PieceMoveMap {
-  return (moves || []).reduce((acc, currMove) => {
-    const pieceIndex = getPieceIndex(currMove.pieceType, currMove.fromSpace);
-    const existingMoves = acc[pieceIndex] || [];
-    return {
-      ...acc,
-      [pieceIndex]: [...existingMoves, currMove]
-    };
-  }, {} as PieceMoveMap);
+  const pieceMoveMap = (moves || [])
+    .reduce((acc, currMove) => {
+      const pieceIndex = getPieceIndex(currMove.pieceType, currMove.fromSpace);
+      const existingMoves = acc[pieceIndex] || [];
+      return {
+        ...acc,
+        [pieceIndex]: [...existingMoves, currMove]
+      };
+    }, {} as PieceMoveMap);
+
+  Object.keys(pieceMoveMap).forEach(pieceIndex => {
+    pieceMoveMap[pieceIndex].sort((a, b) => {
+      const moveTypeOrder = compareMoveTypes(a.moveType, b.moveType);
+      if (moveTypeOrder !== 0) return moveTypeOrder;
+
+      const toHashA = getSpaceCoord(a.toSpace);
+      const toHashB = getSpaceCoord(b.toSpace);
+
+      return toHashA.localeCompare(toHashB);
+    });
+  });
+  
+  return pieceMoveMap;
 }
 
 function getMoveName(move: GameMove): string {
   return `${move.moveType} ${getPieceIndex(move.otherPieceType, move.toSpace)}`;
+}
+
+function compareMoveTypes(a: GameMoveType, b: GameMoveType): number {
+  return getMoveTypeOrder(a) - getMoveTypeOrder(b);
+}
+
+function getMoveTypeOrder(moveType: GameMoveType): number {
+  switch (moveType) {
+    case "MOVE":
+      return 3;
+    case "TAKE":
+      return 2;
+    case "PROMOTION":
+      return 1;
+    case "CASTLE":
+      return 4;
+    default:
+      return 5;
+  }
 }
