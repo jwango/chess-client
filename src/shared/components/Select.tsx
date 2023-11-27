417962773bf29b7d2611e88a45de4708f015b125
jwango <joshua.wang.h@gmail.com>
2023-11-27 06:12:39 -0500

    feat: disable listbox if no moves

--- src/shared/components/Select.tsx
+++ src/shared/components/Select.tsx
@@ -28,8 +28,8 @@ export const Select = <T, O extends SelectOption<T>>({ key, options, placeholder
   }, [key]);
 
   return <div className="w-72 relative">
-  <Listbox value={selectedOption} onChange={setSelectedOption}>
-    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left text-base shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-lg hover:bg-white hover:cursor-pointer">
+  <Listbox value={selectedOption} onChange={setSelectedOption} disabled={!options?.length}>
+    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left text-base shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-lg hover:bg-white hover:cursor-pointer disabled:bg-gray-200">
       {selectedOption?.name || placeholder}
     </Listbox.Button>
     <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-lg shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
