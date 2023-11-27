fb0ec4c9c536f6c6e1141ab500432f955915f914
jwango <joshua.wang.h@gmail.com>
2023-11-26 22:03:22 -0500

    feat: add overflow and max height to DebugQuery, add shared Select component

---
+++ src/shared/components/Select.tsx
@@ -0,0 +1,51 @@
+import { Listbox } from "@headlessui/react";
+import { useEffect, useState } from "react";
+
+interface SelectProps<T, O extends SelectOption<T>> {
+  key: string;
+  options: O[];
+  placeholder?: string;
+  onSelect?: (val: O) => void;
+}
+
+export interface SelectOption<T> {
+  id: string;
+  name: string;
+  val?: T;
+}
+
+export const Select = <T, O extends SelectOption<T>>({ key, options, placeholder = 'None', onSelect }: SelectProps<T, O>) => {
+  const [selectedOption, setSelectedOption] = useState<O>(null);
+
+  useEffect(() => {
+    onSelect && onSelect(selectedOption);
+  }, [selectedOption]);
+
+  useEffect(() => {
+    if (options?.length > 0) {
+      setSelectedOption(options[0]);
+    }
+  }, [key]);
+
+  return <div className="w-72 relative">
+  <Listbox value={selectedOption} onChange={setSelectedOption}>
+    <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm hover:bg-white hover:cursor-pointer">
+      {selectedOption?.name || placeholder}
+    </Listbox.Button>
+    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+      {(options || []).map((option) => (
+        <Listbox.Option
+          key={option.id}
+          value={option}
+          className={({ active }) => 
+            `px-2 py-2 hover:cursor-pointer
+            ${active ? 'bg-blue-200 text-blue-800' : 'bg-white text-black'}`
+          }
+        >
+          {option.name}
+        </Listbox.Option>
+      ))}
+    </Listbox.Options>
+  </Listbox>
+</div>;
+}
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,3 +2,5 @@ export { Panel } from './Panel';
 export { Toast } from './Toast';
 export { getEnv } from './env';
 export { DebugQuery } from './components/DebugQuery';
+export { Select } from './components/Select';
+export type { SelectOption } from './components/Select';
